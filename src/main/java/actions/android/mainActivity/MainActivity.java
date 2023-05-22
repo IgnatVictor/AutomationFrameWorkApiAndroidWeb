6cce106a8cf62cb9bc2b39f389bac1d883660735
ignat victor <meerr67@gmail.com>
2023-05-22 09:00:20 +0300

    add MainActivity and actions

---
+++ src/main/java/actions/android/mainActivity/MainActivity.java
@@ -0,0 +1,56 @@
+package actions.android.mainActivity;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public final class MainActivity {
+
+    private final WebDriver driver;
+
+    private WebElement email;
+    private WebElement loginButton;
+    private WebElement password;
+    private WebElement wrongEmailMessage;
+
+    private WebElement getWrongEmailMessage() {
+        wrongEmailMessage = driver.findElement(By.xpath("//android.widget.Toast[1]"));
+        return  wrongEmailMessage;
+    }
+
+    public MainActivity(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    private WebElement getEmail() {
+        email =  driver.findElement(By.id("inEmail"));
+        return email;
+    }
+
+    private WebElement getPassword() {
+        password =  driver.findElement(By.id("inPassword"));
+        return password;
+    }
+
+    private WebElement getLoginButton() {
+        loginButton =   driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.Button"));
+        return loginButton;
+    }
+
+
+    public void setEmail(String email) {
+        getEmail().sendKeys(email);
+    }
+
+    public void setPassword(String password) {
+        getPassword().sendKeys(password);
+    }
+
+    public void clickOnLoginButton() {
+        getLoginButton().click();
+    }
+
+    public String getWrongEmailMessageText() {
+        return getWrongEmailMessage().getAttribute("name");
+    }
+}
