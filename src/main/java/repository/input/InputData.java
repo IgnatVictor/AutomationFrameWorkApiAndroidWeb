2d4f7d42adf89a6ad517ed4426374b1f31dd047f
ignat victor <meerr67@gmail.com>
2023-05-22 09:03:50 +0300

    add InputData

---
+++ src/main/java/repository/input/InputData.java
@@ -0,0 +1,8 @@
+package repository.input;
+
+public final class InputData {
+
+    public static final String EMAIL = "probaaa@me.com";
+    public static final String PASSWORD = "parola12311";
+    public static final String WRONG_EMAIL = "wrongEmail";
+}
