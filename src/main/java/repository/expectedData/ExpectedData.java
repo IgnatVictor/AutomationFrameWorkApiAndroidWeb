19e0f2d0fd1f5d4f22d0e648c3a74f74c798e0fb
ignat victor <meerr67@gmail.com>
2023-05-22 09:03:33 +0300

    add ExpectedData

---
+++ src/main/java/repository/expectedData/ExpectedData.java
@@ -0,0 +1,10 @@
+package repository.expectedData;
+
+public final class ExpectedData {
+
+    public static final String SECOND_PAGE_TITLE = "second activity page";
+    public static final String WRONG_EMAIL_MESSAGE = "email is not correct";
+    public static final String FIRST_COMMENT = "comment 1";
+    public static final String SECOND_COMMENT = "comment 2";
+    public static final String THIRD_COMMENT = "comment 3";
+}
