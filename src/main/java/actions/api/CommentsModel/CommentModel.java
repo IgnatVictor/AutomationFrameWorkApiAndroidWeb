25304e5f9c8db03ed32abed9e753b449bc42823d
ignat victor <meerr67@gmail.com>
2023-05-22 09:01:26 +0300

    add CommentModel

---
+++ src/main/java/actions/api/CommentsModel/CommentModel.java
@@ -0,0 +1,28 @@
+package actions.api.CommentsModel;
+
+public class CommentModel {
+
+    private int id;
+    private String comment;
+
+    public CommentModel(int id, String comment) {
+        this.id = id;
+        this.comment = comment;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
