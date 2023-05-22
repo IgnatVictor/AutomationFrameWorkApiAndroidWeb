ccd87508ad62d650b42da1f1e642a1524de22691
ignat victor <meerr67@gmail.com>
2023-05-22 09:04:10 +0300

    add ExpectedDataModel

---
+++ src/main/java/repository/model/ExpectedDataModel.java
@@ -0,0 +1,15 @@
+package repository.model;
+
+public class ExpectedDataModel {
+
+    private String expectedData;
+
+    public ExpectedDataModel(String expectedData) {
+        this.expectedData = expectedData;
+    }
+
+    public String getExpectedDataNameField() {
+        return expectedData;
+    }
+}
+
