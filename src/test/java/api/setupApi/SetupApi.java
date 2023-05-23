2b62b5d5a24eb018e43edb1460f3461295aafee2
ignat victor <meerr67@gmail.com>
2023-05-23 15:01:25 +0300

    refactor SetupApi

--- src/test/java/api/setupApi/SetupApi.java
+++ src/test/java/api/setupApi/SetupApi.java
@@ -5,11 +5,9 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import repository.RepositoryApplication;
 
 public abstract class SetupApi {
+    protected SetupApi() throws JsonProcessingException {
+    }
 
     protected CommentsController controller = new CommentsController();
     protected RepositoryApplication repositoryApplication = new RepositoryApplication();
-
-
-    public SetupApi() throws JsonProcessingException {
-    }
 }
