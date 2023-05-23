1d4f50d284017445fa2a8632f95e5e0315598f43
ignat victor <meerr67@gmail.com>
2023-05-23 14:58:09 +0300

    refactor action in  CommentsApi

--- src/main/java/actions/api/user/CommentsApi.java
+++ src/main/java/actions/api/user/CommentsApi.java
@@ -3,35 +3,23 @@ package actions.api.user;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
-import java.util.ArrayList;
-
 public class CommentsApi {
-
     private JsonObject jsonObject;
 
     public JsonObject getComment(String comment, int id) {
         this.jsonObject = new JsonObject();
-        this.jsonObject.add("comment",new Gson().toJsonTree(comment));
-        this.jsonObject.add("id",new Gson().toJsonTree(id));
+        this.jsonObject.add("comment", new Gson().toJsonTree(comment));
+        this.jsonObject.add("id", new Gson().toJsonTree(id));
 
         return this.jsonObject;
     }
 
-    public ArrayList<JsonObject> getAllComments(ArrayList<String[]> listOfAllCommentsFromService) {
-        int id = 0;
-        int comment = 1;
-        ArrayList<JsonObject> listOfComments =  new ArrayList<>();
+    public String[] getAllComments(String[] listOfAllCommentsFromService) {
+        String[] listOfComments = new String[3];
 
-        for(int i=0; i<listOfAllCommentsFromService.size(); i++) {
-            jsonObject =  new JsonObject();
-            this.jsonObject.add("id",new Gson().toJsonTree(listOfAllCommentsFromService.get(i)[id]));
-            this.jsonObject.add("comment",new Gson().toJsonTree(listOfAllCommentsFromService.get(i)[comment]));
-
-
-            listOfComments.add(jsonObject);
+        for (int i = 0; i < listOfAllCommentsFromService.length; i++) {
+            listOfComments[i] = listOfAllCommentsFromService[i];
         }
         return listOfComments;
     }
-
-
 }
