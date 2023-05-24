d6d0c14111d479e4f329968d228980e62852f287
ignat victor <meerr67@gmail.com>
2023-05-24 11:09:44 +0300

    refactor EmailTestCase

--- src/test/java/android/EmailTestCase.java
+++ src/test/java/android/EmailTestCase.java
@@ -2,11 +2,15 @@ package android;
 
 
 import actions.android.mainActivity.MainActivityObject;
-import android.setupAndroid.SetupAndroid;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import common.BaseSetup;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class EmailTestCase extends SetupAndroid {
+public class EmailTestCase extends BaseSetup {
+
+    protected EmailTestCase() throws JsonProcessingException {
+    }
 
     private MainActivityObject createMainActivityObject() {
         return new MainActivityObject(driver);
@@ -14,7 +18,6 @@ public class EmailTestCase extends SetupAndroid {
 
     @Test
     public void checkIfMessagePopsWhenInsertingWrongMailOrWrongFormat() throws InterruptedException {
-
         final MainActivityObject mainActivityObject = createMainActivityObject();
 
         mainActivityObject.setEmail(repositoryApplication.getWrongEmail());
