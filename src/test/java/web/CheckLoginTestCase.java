ed79ea3bb6fa393fc1fe3d7b120ea2ceffe29712
ignat victor <meerr67@gmail.com>
2023-05-24 11:12:00 +0300

    refactor CheckLoginTestCase

--- src/test/java/web/CheckLoginTestCase.java
+++ src/test/java/web/CheckLoginTestCase.java
@@ -2,11 +2,25 @@ package web;
 
 import actions.web.loginPage.LoginPageObject;
 import actions.web.mainPage.MainPageObject;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import common.BaseSetup;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import web.setupWeb.SetupWeb;
+import repository.RepositoryApplication;
 
-public class CheckLoginTestCase extends SetupWeb {
+import java.time.Duration;
+
+public class CheckLoginTestCase extends BaseSetup {
+
+    protected CheckLoginTestCase() throws JsonProcessingException {
+    }
 
     private LoginPageObject createLoginPage() {
         return new LoginPageObject(webDriver);
@@ -14,6 +28,28 @@ public class CheckLoginTestCase extends SetupWeb {
 
     private MainPageObject createMainPage() {
         return new MainPageObject(webDriver);
+    }
+
+    @BeforeMethod
+    @Override
+    public void setup() {
+        final String URL = "https://www.saucedemo.com";
+        repositoryApplication = new RepositoryApplication();
+        ChromeOptions options = new ChromeOptions().addArguments("--remote-allow-origins=*");
+        webDriver = new ChromeDriver(options);
+
+        actionsObject = new Actions(webDriver);
+        wait = new FluentWait<>(webDriver).withTimeout(Duration.ofSeconds(30L)).pollingEvery(Duration.ofSeconds(5L)).ignoring(NoSuchElementException.class);
+
+        webDriver.manage().window().maximize();
+        webDriver.navigate().to(URL);
+    }
+
+    @AfterMethod
+    @Override
+    public void tearDown() {
+        webDriver.close();
+        webDriver.quit();
     }
 
     @Test
