708742ee1b2a28f11270e012c0beff0fcca5071d
ignat victor <meerr67@gmail.com>
2023-05-26 14:12:00 +0300

    rfactor imports

--- src/test/java/web/CheckLoginTestCase.java
+++ src/test/java/web/CheckLoginTestCase.java
@@ -2,21 +2,9 @@ package web;
 
 import actions.web.loginPage.LoginPageObject;
 import actions.web.mainPage.MainPageObject;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import common.BaseSetup;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.FluentWait;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import repository.RepositoryApplication;
 import web.setupWeb.SetupWeb;
-
-import java.time.Duration;
 
 public class CheckLoginTestCase extends SetupWeb {
 
